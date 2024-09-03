8b22aab292fa9834a78db37fb85cdebab5b99ff6
Aditya Singh <aditya.webplat@gmail.com>
2024-09-03 19:01:41 +0530

    Update SdkRpcV2CborGeneratorTest.java

--- core/protocols/smithy-rpcv2-protocol/src/test/java/software/amazon/awssdk/protocols/rpcv2/internal/SdkRpcV2CborGeneratorTest.java
+++ core/protocols/smithy-rpcv2-protocol/src/test/java/software/amazon/awssdk/protocols/rpcv2/internal/SdkRpcV2CborGeneratorTest.java
@@ -32,117 +32,112 @@ class SdkRpcV2CborGeneratorTest {
 
     @ParameterizedTest(name = "{index} - {0}")
     @MethodSource("testCases")
-    public void runTestCases(TestCase testCase) {
-        StructuredJsonGenerator generator = getGenerator();
-        testCase.setup.accept(generator);
-        assertThat(toHexEncoded(generator.getBytes())).isEqualTo(annotatedToHex(testCase.expected));
-    }
+    static Collection<TestCase> testCases() {
+    return Arrays.asList(
+        // Numbers, minimal encoding
+        builder("Minimally encodes longs when they fit as a simple value")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(23L)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=8117"
+                , "81    # array(1)"
+                , "   17 #   unsigned(23)"
+            )
+            .build()
+        , builder("Minimally encodes longs when they fit in one byte")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(255L)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=8118ff"
+                , "81       # array(1)"
+                , "   18 ff #   unsigned(255)"
+            )
+            .build()
+        , builder("Minimally encodes longs when they fit in two byte")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(256L)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=81190100"
+                , "81         # array(1)"
+                , "   19 0100 #   unsigned(256)"
+            )
+            .build()
+        , builder("Minimally encodes floats when are equivalent to ints")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(23.0F)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=8117"
+                , "81    # array(1)"
+                , "   17 #   unsigned(23)"
+            )
+            .build()
+        , builder("Minimally encodes doubles when are equivalent to ints")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(23.0)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=8117"
+                , "81    # array(1)"
+                , "   17 #   unsigned(23)"
+            )
+            .build()
+        , builder("Minimally encodes doubles that fit in a float")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(1.5)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=81fa3fc00000"
+                , "81             # array(1)"
+                , "   fa 3fc00000 #   float(1.5)"
+            )
+            .build()
 
-    public static Collection<TestCase> testCases() {
-        return Arrays.asList(
-            // Numbers, minimal encoding
-            builder("Minimally encodes longs when they fit as a simple value")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(23L)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=8117"
-                    , "81    # array(1)"
-                    , "   17 #   unsigned(23)"
-                )
-                .build()
-            , builder("Minimally encodes longs when they fit in one byte")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(255L)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=8118ff"
-                    , "81       # array(1)"
-                    , "   18 ff #   unsigned(255)"
-                )
-                .build()
-            , builder("Minimally encodes longs when they fit in two byte")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(256L)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=81190100"
-                    , "81         # array(1)"
-                    , "   19 0100 #   unsigned(256)"
-                )
-                .build()
-            , builder("Minimally encodes floats when are equivalent to ints")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(23.0F)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=8117"
-                    , "81    # array(1)"
-                    , "   17 #   unsigned(23)"
-                )
-                .build()
-            , builder("Minimally encodes doubles when are equivalent to ints")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(23.0)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=8117"
-                    , "81    # array(1)"
-                    , "   17 #   unsigned(23)"
-                )
-                .build()
-            , builder("Minimally encodes doubles that fit in a float")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(1.5)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=81fa3fc00000"
-                    , "81             # array(1)"
-                    , "   fa 3fc00000 #   float(1.5)"
-                )
-                .build()
+        , builder("Encodes doubles as doubles when needed")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(3.1415927)
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=81fb400921fb5a7ed197"
+                , "81                     # array(1)"
+                , "   fb 400921fb5a7ed197 #   float(3.1415927)"
+            )
+            .build()
+        // Timestamp as epoch seconds with milliseconds decimal part
+        , builder("Encodes timestamp as epoch seconds with millisecond decimal part")
+            .setup(
+                g -> g.writeStartArray(1)
+                      .writeValue(Instant.parse("2024-08-09T18:13:18.426482Z"))
+                      .writeEndArray()
+            )
+            .expected(
+                "# https://cbor.nemo157.com/#type=hex&value=81C1FB41D9AD970F9B4396"
+                , "81                        # array(1)"
+                , "   c1                     #   epoch datetime value, tag(1)"
+                , "      fb 41d9ad970f9b4396 #     float(1,723,227,198.426)"
+                , "                          #     datetime(2024-08-09T18:13:18.426000118Z)"
+            )
+            .build()
+    );
+}
 
-            , builder("Encodes doubles as doubles when needed")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(3.1415927)
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=81fb400921fb5a7ed197"
-                    , "81                     # array(1)"
-                    , "   fb 400921fb5a7ed197 #   float(3.1415927)"
-                )
-                .build()
-            // Timestamp as epoch seconds with milliseconds decimal part
-            , builder("Encodes timestamp as epoch seconds with millisecond decimal part")
-                .setup(
-                    g -> g.writeStartArray(1)
-                          .writeValue(Instant.parse("2024-08-09T18:13:18.426482Z"))
-                          .writeEndArray()
-                )
-                .expected(
-                    "# https://cbor.nemo157.com/#type=hex&value=81C1FB41D9AD970F9B4396"
-                    , "81                        # array(1)"
-                    , "   c1                     #   epoch datetime value, tag(1)"
-                    , "      fb 41d9ad970f9b4396 #     float(1,723,227,198.426)"
-                    , "                          #     datetime(2024-08-09T18:13:18.426000118Z)"
-                )
-                .build()
-        );
-    }
 
     static TestCaseBuilder builder(String name) {
         return new TestCaseBuilder()
